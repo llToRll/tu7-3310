3427f377ec1bd7a1872adf11c12eb4b11245eb29
Administrator <Administrator@ACH5VT9IMH9RFPX>
2012-09-22 16:52:12 +0700

    add MyForm

---
+++ MobileApplication1/src/MyForm.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.microedition.lcdui.*;
+/**
+ *
+ * @author Administrator
+ */
+public class MyForm extends Form {
+    public MyForm(){
+        super("Hello world!!!");
+    }
+}
