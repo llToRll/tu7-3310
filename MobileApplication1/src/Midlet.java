178ed08b4dcd413eeb6f0670f4176d893852f9d4
Tor <Tor@Tor-PC>
2012-09-22 16:42:26 +0700

    ggg

---
+++ MobileApplication1/src/Midlet.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author Tor
+ */
+public class Midlet extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
