4eafb1cf6896b1b2aa36f4e1763fc4b6c803e583
Administrator <Administrator@ACH5VT9IMH9RFPX>
2012-09-22 16:59:12 +0700

    Change myform

--- MobileApplication1/src/MyForm.java
+++ MobileApplication1/src/MyForm.java
@@ -7,8 +7,14 @@ import javax.microedition.lcdui.*;
  *
  * @author Administrator
  */
-public class MyForm extends Form {
+public class MyForm extends Form implements CommandListener{
     public MyForm(){
         super("Hello world!!!");
     }
+
+    public void commandAction(Command c, Displayable d) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    
 }
